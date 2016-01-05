7a6a836d2ab8329690b9b8cf1424ff81dd8c5add
robinchoii <robinchoii@gmail.com>
2016-01-04 17:30:58 -0800

    nimgame solution js

---
+++ NimGame.js
@@ -0,0 +1,21 @@
+// You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
+
+// Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win the game given the number of stones in the heap.
+
+var canWinNim = function(n) {
+    if (n == 0) {
+        return false
+    } else if (n == 1 || n == 2 || n == 3) {
+        return true
+    } else if (n % 4 == 0) {
+        return false
+    } else if (n % 4 == 1) {
+        return true
+    } else if (n % 4 == 2) {
+        return true
+    } else if (n % 4 == 3) {
+        return true
+    } else {
+        console.log("Please enter a whoele number")
+    }
+};
