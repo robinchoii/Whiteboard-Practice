1c0d5d111622980a19d33ce823e6929b2436c43e
robinchoii <robinchoii@gmail.com>
2016-02-11 22:13:53 -0800

    add staircase solution

--- hello.rb
+++
@@ -1,6 +0,0 @@
-def greetings
-  p "hello world!"
-end
-greetings
-
-
---
+++ staircase.rb
@@ -0,0 +1,13 @@
+def stair_case(n) # n = 6
+  stair = "#"
+  n.times do
+    puts stair
+    stair = stair + "#"
+  end
+end
+
+#
+##
+###
+####
+#####
